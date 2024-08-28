c6f9ae048292da9cb322a9f5a5f1a915900a8521
author <email@example.com>
2024-08-28 16:16:45 +0200

    Create SetupFixture.cs

---
+++ GHPluginTests/GHPluginRH8Tests1/SetupFixture.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace GHPluginRH8Tests1
+{
+    [SetUpFixture]
+    public sealed class SetupFixture : Rhino.Testing.Fixtures.RhinoSetupFixture
+    {
+        public override void OneTimeSetup()
+        {
+            base.OneTimeSetup();
+            // your custom setup
+        }
+
+        public override void OneTimeTearDown()
+        {
+            base.OneTimeTearDown();
+            // you custom teardown
+        }
+    }
+}
