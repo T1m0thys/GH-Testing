cbc3906498d68f1c8eb435787f6e9f35cfc97947
author <email@example.com>
2024-08-28 15:16:51 +0200

    Adding template Project

---
+++ GHPluginTests/GHPluginRH8/GHPluginRH8Info.cs
@@ -0,0 +1,26 @@
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace GHPluginRH8
+{
+    public class GHPluginRH8Info : GH_AssemblyInfo
+    {
+        public override string Name => "GHPluginRH8";
+
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        public override Bitmap Icon => null;
+
+        //Return a short string describing the purpose of this GHA library.
+        public override string Description => "";
+
+        public override Guid Id => new Guid("7599a5b5-845a-4075-bb9d-5d6e33ad58be");
+
+        //Return a string identifying you or your company.
+        public override string AuthorName => "";
+
+        //Return a string representing your preferred contact details.
+        public override string AuthorContact => "";
+    }
+}
