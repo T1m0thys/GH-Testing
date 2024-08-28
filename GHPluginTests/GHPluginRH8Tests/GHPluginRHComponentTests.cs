cef5b601e48b43d251a2b00b0d91d473d0ef0d03
author <email@example.com>
2024-08-28 15:28:42 +0200

    Solving Build Errors
    Adding to `*.csproj`:
     - `<PackageReference Include="System.Drawing.Common" Version="8.0.8" />`

---
+++ GHPluginTests/GHPluginRH8Tests/GHPluginRHComponentTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace GHPluginRH8.Tests
+{
+    [TestFixture()]
+    public class GHPluginRHComponentTests
+    {
+        [Test()]
+        public void CreateSpiralTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
